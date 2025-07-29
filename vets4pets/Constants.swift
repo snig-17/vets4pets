d53f1375f637f3d509ef8dc9dccbfda5aafcdaf7
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-29 14:18:26 +0530

    added files

---
+++ vets4pets/Constants.swift
@@ -0,0 +1,41 @@
+//
+//
+//  Constants.swift
+//  vets4pets
+//
+//  Created by Snigdha Tiwari on 01/02/24.
+//
+
+import Foundation
+
+let REALM_QUEUE = DispatchQueue(label: "realmQueue") //global variable
+let REALM = "realRunConfig"
+
+struct K {
+ 
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+  static let appName = "Vets4Pets🐶"
+    
+    static let moreDetailsSegue = "toMoreDetails"
+    static let adminSegue = "toAdminHome"
+}
+
+
+// End of Constants.swift
---
+++ vets4pets/MoreDetailsViewController.swift
@@ -0,0 +1,22 @@
+// Start of MoreDetailsViewController.swift
+//
+//  MoreDetailsViewController.swift
+//  vets4pets
+//
+//  Created by Snigdha Tiwari on 01/02/24.
+//
+
+import UIKit
+
+class MoreDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
+
+
+// End of MoreDetailsViewController.swift
